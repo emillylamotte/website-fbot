c50d4a360864be74a7c20fb634662e47dd6be0af
Emilly Lamotte <emilly.lamotte@furg.br>
2021-08-26 03:10:12 -0300

    Update website - com slider funcionando

---
+++ controller.js
@@ -0,0 +1,12 @@
+/*Função para scroll entre seções da página*/ 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+
+
